e13e070a717e60ae770539ad558975cefc6ba173
Tzvetomir Kotzev <tzvetomirkotzev@Tzvetomirs-MacBook-Air.local>
2022-11-16 00:00:16 -0500

    This was hard - all bonuses included/last bonus sucked

--- index.js
+++ index.js
@@ -30,12 +30,49 @@
 let selectedColor = 'blue'
 const painting = document.querySelector('.painting')
 const palette = document.querySelector('.palette')
+let clearButton = document.createElement('button')
 
+//append purple onto palette
+let colorPurple = document.createElement('div')
+colorPurple.setAttribute('class', 'color-choice')
+colorPurple.setAttribute('id', 'purple')
+colorPurple.textContent = '■'
+palette.append(colorPurple)
+
+//button properties and styling
+clearButton.textContent = 'Click to clear painting'
+clearButton.style.backgroundColor = 'black'
+clearButton.style.color = 'white'
+clearButton.setAttribute('id', 'white')
+console.log(clearButton.id)
+document.body.append(clearButton)
+
+//changing the color from teh pallete
+palette.addEventListener('click', function(e){
+	console.log(e.target)
+	selectedColor = e.target.id
+	console.log(selectedColor)
+//	selectedColor = e.target.color
+})
+
+//painting with selected color
 painting.addEventListener('click', function(e){
 	e.target.style.backgroundColor = selectedColor 
+	console.log(e.target)
+	//clearing teh painting with the button
+	clearButton.addEventListener('click', function(){
+		e.target.style.backgroundColor = 'white'
+	})
 })
 
 
-painting.addEventListener('click', function(e){
-	e.target.style.backgroundColor = selectedColor 
+//doublecking to turn pixel back to white
+painting.addEventListener('dblclick', function(e){
+	e.target.style.backgroundColor = 'white' 
 })
+
+
+
+
+
+
